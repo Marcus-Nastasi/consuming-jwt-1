c356ad64961601ce406ec1b02fad01b824c18de1
rolemberg <vinnie.nsts@gmail.com>
2024-04-26 01:32:15 -0300

    Made App inicial config

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,48 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+   const [ responses, setResponses ] = useState([]);
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+   async function handleSubmit(e: any): Promise<void> {
+      e.preventDefault();
+      
+      const inputUser: any = document.getElementById('user');
+      const inputPassword: any = document.getElementById('password');
+
+      const url: string = "http://localhost:3001/login";
+      const config: object = {
+         method: "post", 
+         body: JSON.stringify({ user: inputUser.value, password: inputPassword.value })
+      };
+
+      try {
+         const request = await fetch(url, config);
+         const response = await request.json();
+         console.log(response);
+      } catch(e) {
+         console.log("error: " + e)
+      };
+   };
+
+   return (
+      <>
+         <section>
+
+            <form onSubmit={handleSubmit} method="post">
+
+               <label htmlFor="user">Login</label>
+               <input type="text" name="user" id="user" />
+
+               <label htmlFor="password">Password</label>
+               <input type="password" name="password" id="password" />
+
+               <button type="submit">send</button>
+
+            </form>
+
+         </section>
+      </>
   )
-}
+};
 
-export default App
+
