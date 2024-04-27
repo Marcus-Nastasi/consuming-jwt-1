a0a4a8bc13a894788173cfa2d6a9efcacd64edcf
rolemberg <vinnie.nsts@gmail.com>
2024-04-27 11:53:34 -0300

    successfuly done

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,38 @@
-import { StrictMode, useState } from "react";
+import { useState } from "react";
 
 export default function App() {
-   const [ responses, setResponses ] = useState([]);
+   const [ token, setToken ] = useState('');
+   const [ resp, setResp ] = useState([]);
 
    async function handleSubmit(e: any): Promise<void> {
       e.preventDefault();
       
-      const inputUser: any = document.getElementById('user');
-      const inputPassword: any = document.getElementById('password');
+      const response = await fetch('http://localhost:3001/api', {
+         headers: new Headers({ 'x-access-token': token })
+      });
+      const converted = await response.json();
 
-      const url: string = "http://localhost:3001/api";
-      // const config: object = {
-      //    method: "post", 
-      //    body: 
-      // };
-
-      try {
-         const request: Response = await fetch(url)
-         const response: any = await request.json();
-         setResponses(response);
-         console.log(response);
-      } catch(e) {
-         console.log("error: " + e)
-      };
+      setResp(converted);
    };
+
+   async function postToToken(e: any): Promise<void> {
+      e.preventDefault();
+
+      const user: any = document.getElementById('user');
+      const pass: any = document.getElementById('password');
+
+      const url: string = 'http://localhost:3001/login';
+      const options: object = {
+         method: 'post',
+         body: JSON.stringify({ user: user.value, password: pass.value }),
+         headers: new Headers({'content-type': 'application/json', })
+      };
+
+      const req = await fetch(url, options);
+      const res = await req.json();
+
+      setToken(res.token);
+   }
 
    return (
       <>
@@ -37,11 +46,13 @@ export default function App() {
                <label htmlFor="password">Password</label>
                <input type="password" name="password" id="password" />
 
-               <button  type="submit">send</button>
+               <button onClick={postToToken} type="submit">send</button>
 
             </form>
 
             <button onClick={handleSubmit}>API</button>
+
+            {resp.map((r: any) => <p key={r.id}> {r.description} </p>)}
 
          </section>
       </>
