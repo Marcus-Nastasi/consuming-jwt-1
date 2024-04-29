34df05b2d8a295fd7863804aff84d8a238f6c136
rolemberg <vinnie.nsts@gmail.com>
2024-04-29 02:59:32 -0300

    added styles;

--- src/App.tsx
+++ src/App.tsx
@@ -31,7 +31,7 @@ export default function App() {
       const response: Response = await fetch('http://localhost:3001/api', {
          headers: new Headers({ 'x-access-token': token })
       });
-      
+
       const converted: Array<Task> = await response.json();
 
       setResp(converted);
@@ -39,27 +39,73 @@ export default function App() {
 
    return (
       <>
-         <section>
+         <section style={topSectionStyle}>
+            <section style={middleSectionStyle}>
+               <form style={formStyle}>
 
-            <form>
+                  <label style={labelStyle} htmlFor="user">Login</label>
+                  <input style={inputStyle} type="text" name="user" id="user" />
 
-               <label htmlFor="user">Login</label>
-               <input type="text" name="user" id="user" />
+                  <label style={labelStyle} htmlFor="password">Password</label>
+                  <input style={inputStyle} type="password" name="password" id="password" />
 
-               <label htmlFor="password">Password</label>
-               <input type="password" name="password" id="password" />
+                  <button style={formButton} onClick={handleTokenSubmit} type="submit">send</button>
 
-               <button onClick={handleTokenSubmit} type="submit">send</button>
+               </form>
 
-            </form>
+               <button style={formButton} onClick={handleGetApi}>API</button>
 
-            <button onClick={handleGetApi}>API</button>
-
-            {resp.map((r: any) => <p key={r.id}> {r.description} </p>)}
-
+               {resp.map((r: Task) => <p key={String(r.id)}> {r.description} </p>)}
+            </section>
          </section>
       </>
   )
 };
 
+const topSectionStyle: object = {
+   display: 'flex',
+   justifyContent: 'center',
+   alignItems: 'center',
+   backgroundColor: '#0093E9',
+   backgroundImage: 'linear-gradient(160deg, #0093E9 0%, #80D0C7 100%)',
+   height: '100vh'
+};
+
+const middleSectionStyle: object = {
+   display: 'flex',
+   flexDirection: 'column',
+   backgroundColor: '#8EC5FC',
+   backgroundImage: 'linear-gradient(62deg, #8EC5FC 0%, #E0C3FC 100%)',
+   padding: '2rem',
+   height: '50vh',
+   width: '40%',
+   borderRadius: '2rem'
+};
+
+const formStyle: object = {
+   display: 'flex',
+   flexDirection: 'column',
+   justifyContent: 'space-evenly',
+   height: '80%',
+   fontSize: '2.2rem'
+};
+
+const labelStyle: object = {
+   color: 'black'
+};
+
+const inputStyle: object = {
+   padding: '0.5rem',
+   borderRadius: '2rem'
+};
+
+const formButton: object = {
+   width: '20rem',
+   padding: '0.3rem',
+   marginTop: '1rem',
+   fontSize: '1rem',
+   cursor: 'pointer',
+   alignSelf: 'center',
+   borderRadius: '0.5rem'
+};
 
